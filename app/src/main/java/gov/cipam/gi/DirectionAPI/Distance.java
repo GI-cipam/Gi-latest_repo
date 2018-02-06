6d87f4975ff4ff0228b5207ae18817a9f611785f
Nitant96 <nsgm3a@gmail.com>
2018-02-07 02:15:16 +0530

    Initial Notification Implemented-1

---
+++ app/src/main/java/gov/cipam/gi/DirectionAPI/Distance.java
@@ -0,0 +1,14 @@
+package gov.cipam.gi.DirectionAPI;
+
+/**
+ * Created by Nitant Sood on 1/02/2018.
+ */
+public class Distance {
+    public String text;
+    public int value;
+
+    public Distance(String text, int value) {
+        this.text = text;
+        this.value = value;
+    }
+}
---
+++ app/src/main/java/gov/cipam/gi/DirectionAPI/Route.java
@@ -0,0 +1,19 @@
+package gov.cipam.gi.DirectionAPI;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+
+/**
+ * Created by Nitant Sood on 1/02/2018.
+ */
+public class Route {
+    public Distance distance;
+    public Duration duration;
+    public String endAddress;
+    public LatLng endLocation;
+    public String startAddress;
+    public LatLng startLocation;
+
+    public List<LatLng> points;
+}
