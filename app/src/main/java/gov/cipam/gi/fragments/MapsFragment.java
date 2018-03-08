5cf4ad34d506fc7333451653a50a570c7bc969ca
karan <karan26796@gmail.com>
2018-03-09 01:18:11 +0530

    bottom_sheet_fab_added

--- app/src/main/java/gov/cipam/gi/fragments/MapsFragment.java
+++ app/src/main/java/gov/cipam/gi/fragments/MapsFragment.java
@@ -6,15 +6,19 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -49,28 +53,29 @@ import gov.cipam.gi.utils.Constants;
  */
 
 public class MapsFragment extends Fragment implements OnMapReadyCallback
-        ,GoogleMap.OnInfoWindowClickListener
-        ,MapsSellerAdapter.setOnMapSellerClickedListener{
+        , GoogleMap.OnInfoWindowClickListener
+        , MapsSellerAdapter.setOnMapSellerClickedListener
+,View.OnClickListener{
 
     MapView mapView;
+    FloatingActionButton fabBottomSheet;
     GoogleMap map;
     RecyclerView rvSellers;
     LinearLayout layoutBottomSheet;
     ArrayList<Seller> selectedSellerList;
-    ArrayList<Seller> allSellerList=new ArrayList<>();
+    ArrayList<Seller> allSellerList = new ArrayList<>();
     BottomSheetBehavior sheetBehavior;
     Database databaseInstance;
     SQLiteDatabase database;
-    public static boolean hasSpecializedList=false;
+    public static boolean hasSpecializedList = false;
 
     public static MapsFragment newInstance(ArrayList<Seller> seletedSellerList) {
 
         Bundle args = new Bundle();
-        if(seletedSellerList!=null){
-            args.putSerializable("selectedSellerList",seletedSellerList);
-        }
-        else{
-            args.putSerializable("selectedSellerList",null);
+        if (seletedSellerList != null) {
+            args.putSerializable("selectedSellerList", seletedSellerList);
+        } else {
+            args.putSerializable("selectedSellerList", null);
         }
         MapsFragment fragment = new MapsFragment();
         fragment.setArguments(args);
@@ -87,16 +92,44 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback
         super.onViewCreated(view, savedInstanceState);
         databaseInstance = new Database(getContext());
         database = databaseInstance.getReadableDatabase();
-
-        layoutBottomSheet=view.findViewById(R.id.bottom_sheet);
+        fabBottomSheet=view.findViewById(R.id.button_bottom_sheet);
+        layoutBottomSheet = view.findViewById(R.id.bottom_sheet_layout);
         sheetBehavior = BottomSheetBehavior.from(layoutBottomSheet);
-        rvSellers=view.findViewById(R.id.recycler_map_sellers);
+        rvSellers = view.findViewById(R.id.recycler_map_sellers);
 
-        rvSellers.setLayoutManager(new GridLayoutManager(getContext(),2));
+        rvSellers.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
         rvSellers.setAdapter(new MapsSellerAdapter(this));
 
-        SupportMapFragment mapFragment1=(SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapFragment);
+        SupportMapFragment mapFragment1 = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapFragment);
         mapFragment1.getMapAsync(this);
+
+        sheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED: {
+                        fabBottomSheet.setImageResource(R.drawable.ic_expand_more);
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_COLLAPSED: {
+                        fabBottomSheet.setImageResource(R.drawable.ic_expand_less);
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+        fabBottomSheet.setOnClickListener(this);
     }
 
     @Override
@@ -126,21 +159,21 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback
 //            Toast.makeText(getContext(),oneSeller.getUid(), Toast.LENGTH_SHORT).show();
                 Picasso.with(getContext())
                         .load(product.getDpurl())
-                        .resize(200,300).centerCrop()
+                        .resize(200, 300).centerCrop()
                         .into(imageView, new Callback() {
-                    @Override
-                    public void onSuccess() {
-                        if (marker.isInfoWindowShown()) {
-                            marker.hideInfoWindow();
-                            marker.showInfoWindow();
-                        }
-                    }
+                            @Override
+                            public void onSuccess() {
+                                if (marker.isInfoWindowShown()) {
+                                    marker.hideInfoWindow();
+                                    marker.showInfoWindow();
+                                }
+                            }
 
-                    @Override
-                    public void onError() {
+                            @Override
+                            public void onError() {
 
-                    }
-                });
+                            }
+                        });
                 productName.setText(product.getName());
                 sellerNAme.setText(oneSeller.getName());
                 sellerAddress.setText(oneSeller.getaddress());
@@ -179,16 +212,16 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback
 
     private void addMarkerForSelectedSeller() {
         LatLngBounds.Builder builder = new LatLngBounds.Builder();
-        for(int sellerIndex=0;sellerIndex<selectedSellerList.size();sellerIndex++){
-            final Seller currentSeller=selectedSellerList.get(sellerIndex);
-            final LatLng currentLatLng=new LatLng(currentSeller.getlat(),currentSeller.getlon());
-            Product currentProduct=getProduct(currentSeller.getUid());
-            Marker marker=map.addMarker(new MarkerOptions()
+        for (int sellerIndex = 0; sellerIndex < selectedSellerList.size(); sellerIndex++) {
+            final Seller currentSeller = selectedSellerList.get(sellerIndex);
+            final LatLng currentLatLng = new LatLng(currentSeller.getlat(), currentSeller.getlon());
+            Product currentProduct = getProduct(currentSeller.getUid());
+            Marker marker = map.addMarker(new MarkerOptions()
                     .position(currentLatLng)
                     .title(currentSeller.getName())
                     .snippet(currentSeller.getaddress())
                     .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
-            SellerAndView sellerAndView=new SellerAndView(currentSeller,null);
+            SellerAndView sellerAndView = new SellerAndView(currentSeller, null);
             marker.setTag(sellerAndView);
             builder.include(marker.getPosition());
         }
@@ -199,60 +232,60 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback
     }
 
     public Product getProduct(String uidToSearch) {
-    Product product;
-    String s[]={uidToSearch};
-       Cursor cursor=database.query(Database.GI_PRODUCT_TABLE,null,Database.GI_PRODUCT_UID +"=?",s,null,null,null);
+        Product product;
+        String s[] = {uidToSearch};
+        Cursor cursor = database.query(Database.GI_PRODUCT_TABLE, null, Database.GI_PRODUCT_UID + "=?", s, null, null, null);
 //        Cursor cursor=database.query(Database.GI_PRODUCT_TABLE,null,null,null,null,null,null);
 
         cursor.moveToNext();
-            String name,detail,category,state,dpurl,uid,history,description;
+        String name, detail, category, state, dpurl, uid, history, description;
 
-            name=cursor.getString(cursor.getColumnIndex(Database.GI_PRODUCT_NAME));
-            detail=cursor.getString(cursor.getColumnIndex(Database.GI_PRODUCT_DETAIL));
-            category=cursor.getString(cursor.getColumnIndex(Database.GI_PRODUCT_CATEGORY));
-            state=cursor.getString(cursor.getColumnIndex(Database.GI_PRODUCT_STATE));
-            dpurl=cursor.getString(cursor.getColumnIndex(Database.GI_PRODUCT_DP_URL));
-            uid=cursor.getString(cursor.getColumnIndex(Database.GI_PRODUCT_UID));
+        name = cursor.getString(cursor.getColumnIndex(Database.GI_PRODUCT_NAME));
+        detail = cursor.getString(cursor.getColumnIndex(Database.GI_PRODUCT_DETAIL));
+        category = cursor.getString(cursor.getColumnIndex(Database.GI_PRODUCT_CATEGORY));
+        state = cursor.getString(cursor.getColumnIndex(Database.GI_PRODUCT_STATE));
+        dpurl = cursor.getString(cursor.getColumnIndex(Database.GI_PRODUCT_DP_URL));
+        uid = cursor.getString(cursor.getColumnIndex(Database.GI_PRODUCT_UID));
 
-            history=cursor.getString(cursor.getColumnIndex(Database.GI_PRODUCT_HISTORY));
-            description=cursor.getString(cursor.getColumnIndex(Database.GI_PRODUCT_DESCRIPTION));
+        history = cursor.getString(cursor.getColumnIndex(Database.GI_PRODUCT_HISTORY));
+        description = cursor.getString(cursor.getColumnIndex(Database.GI_PRODUCT_DESCRIPTION));
 
-            product = new Product(name,dpurl,detail,category,state,description,history,uid);
+        product = new Product(name, dpurl, detail, category, state, description, history, uid);
         cursor.close();
-            return product;
+        return product;
 
 
     }
 
     private void addMarkerForAllSeller() {
         LatLngBounds.Builder builder = new LatLngBounds.Builder();
-        for(Seller currentSeller : allSellerList){
-            LatLng currentLatLng=new LatLng(currentSeller.getlat(),currentSeller.getlon());
-            Marker marker=map.addMarker(new MarkerOptions().position(currentLatLng).title(currentSeller.getName()).snippet(currentSeller.getaddress()));
-            SellerAndView sellerAndView=new SellerAndView(currentSeller,null);
+        for (Seller currentSeller : allSellerList) {
+            LatLng currentLatLng = new LatLng(currentSeller.getlat(), currentSeller.getlon());
+            Marker marker = map.addMarker(new MarkerOptions().position(currentLatLng).title(currentSeller.getName()).snippet(currentSeller.getaddress()));
+            SellerAndView sellerAndView = new SellerAndView(currentSeller, null);
             marker.setTag(sellerAndView);
             builder.include(marker.getPosition());
         }
         LatLngBounds bounds = builder.build();
-            int padding = 20; // offset from edges of the map in pixels
-            CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
-            map.animateCamera(cu);
+        int padding = 20; // offset from edges of the map in pixels
+        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+        map.animateCamera(cu);
     }
 
     private void inflateSellerListFromDB() {
-        Cursor sellerCursor=database.query(Database.GI_SELLER_TABLE,null,null,null,null,null,null,null);
-        while(sellerCursor.moveToNext()){
-            String name,address,contact,uid;
-            Double lon,lat;
+        Cursor sellerCursor = database.query(Database.GI_SELLER_TABLE, null, null, null, null, null, null, null);
+        while (sellerCursor.moveToNext()) {
+            String name, address, contact, uid;
+            Double lon, lat;
 
-            name=sellerCursor.getString(sellerCursor.getColumnIndex(Database.GI_SELLER_NAME));
-            address=sellerCursor.getString(sellerCursor.getColumnIndex(Database.GI_SELLER_ADDRESS));
-            contact=sellerCursor.getString(sellerCursor.getColumnIndex(Database.GI_SELLER_CONTACT));
-            lat=sellerCursor.getDouble(sellerCursor.getColumnIndex(Database.GI_SELLER_LAT));
-            lon=sellerCursor.getDouble(sellerCursor.getColumnIndex(Database.GI_SELLER_LON));
-            uid=sellerCursor.getString(sellerCursor.getColumnIndex(Database.GI_SELLER_UID));
+            name = sellerCursor.getString(sellerCursor.getColumnIndex(Database.GI_SELLER_NAME));
+            address = sellerCursor.getString(sellerCursor.getColumnIndex(Database.GI_SELLER_ADDRESS));
+            contact = sellerCursor.getString(sellerCursor.getColumnIndex(Database.GI_SELLER_CONTACT));
+            lat = sellerCursor.getDouble(sellerCursor.getColumnIndex(Database.GI_SELLER_LAT));
+            lon = sellerCursor.getDouble(sellerCursor.getColumnIndex(Database.GI_SELLER_LON));
+            uid = sellerCursor.getString(sellerCursor.getColumnIndex(Database.GI_SELLER_UID));
 
-            Seller oneSeller = new Seller(name, address, contact, lon, lat,uid);
+            Seller oneSeller = new Seller(name, address, contact, lon, lat, uid);
 
             allSellerList.add(oneSeller);
         }
@@ -261,12 +294,12 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback
 
     @Override
     public void onInfoWindowClick(Marker marker) {
-        SellerAndView sellerAndView=(SellerAndView) marker.getTag();
-        Seller oneSeller=sellerAndView.getSeller();
-        View infoView=sellerAndView.getView();
-        Product product=getProduct(oneSeller.getUid());
+        SellerAndView sellerAndView = (SellerAndView) marker.getTag();
+        Seller oneSeller = sellerAndView.getSeller();
+        View infoView = sellerAndView.getView();
+        Product product = getProduct(oneSeller.getUid());
 
-        ImageView imageView=infoView.findViewById(R.id.mapInfoWindowImage);
+        ImageView imageView = infoView.findViewById(R.id.mapInfoWindowImage);
 
 //        Bitmap bitmap=null;
 //        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
@@ -292,15 +325,30 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback
 //                .commit();
 
 
-            Bundle bundle = new Bundle();
-            bundle.putSerializable("ppp", product);
-            startActivity(new Intent(getContext(), ProductListActivity.class)
-                    .putExtra(Constants.KEY_TYPE, Database.GI_PRODUCT)
-                    .putExtras(bundle));
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("ppp", product);
+        startActivity(new Intent(getContext(), ProductListActivity.class)
+                .putExtra(Constants.KEY_TYPE, Database.GI_PRODUCT)
+                .putExtras(bundle));
     }
-    
+
     @Override
     public void onMapSellerClicked(MapsSellerAdapter.MapsSellerViewHolder view, int position) {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_bottom_sheet:
+                if (sheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+                    sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                    fabBottomSheet.setImageResource(R.drawable.ic_expand_less);
+                } else {
+                    sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                    fabBottomSheet.setImageResource(R.drawable.ic_expand_more);
+                }
+                break;
+        }
     }
 }
